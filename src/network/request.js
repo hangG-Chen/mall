51afa3c244b7b74b057b01dfa4863fce9462ac0c
hangG-Chen <598991760@qq.com>
2020-05-01 17:51:45 +0800

    2020.5.1

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-04 10:49:31
- * @LastEditTime: 2020-04-09 15:52:41
+ * @LastEditTime: 2020-05-01 17:51:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mall\src\network\request.js
@@ -10,9 +10,7 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    // baseURL : "http://152.136.185.210:8000/api/h8" ,
-    // baseURL : "http://123.207.32.32:8000/api/h8" ,
-    baseURL : "http://106.54.54.237:8000/api/h8"  ,
+    
     timeout: 5000
   });
   instance.interceptors.request.use(config => {
--- src/views/detail/childComps/DetailBaseInfo.vue
+++ src/views/detail/childComps/DetailBaseInfo.vue
@@ -1,31 +1,47 @@
 <!--
  * @Author: your name
  * @Date: 2020-04-10 13:49:28
- * @LastEditTime: 2020-04-13 18:51:39
+ * @LastEditTime: 2020-05-01 17:33:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mall\src\views\detail\childComps\DetailBaseInfo.vue
  -->
 <template>
-  <div v-if="Object.keys(goodsInfo).length!== 0">
-    
+  <div v-if="Object.keys(goodsInfo).length!== 0">
+    <div class="info-title">{{goodsInfo.desc}}</div>
+    <div class="info-price">
+      <div class="n-price">{{goodsInfo.price}}</div>
+      <div class="o-price">{{goodsInfo.oldPrice}}</div>
+    </div>
+    <div class="info-other">
+      <span>{{goodsInfo.columns[0]}}</span>
+      <span>{{goodsInfo.columns[1]}}</span>
+      <span>{{goodsInfo.services[goodsInfo.services.length-1].name}}</span>
+    </div>
+    <div class="info-service">
+      <span class="info-service-item" 
+        v-for="index in goodsInfo.services.length-1" :key="index">
+        <img :src="goodsInfo.services[index-1].icon" alt="">
+        <span>{{goodsInfo.services[index-1].name}}</span>
+      </span>
+      
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "DetailBaseInfo",
-    props: {
-      goodsInfo: {
-        type: Object,
-        default() {
-          return {}
-        }
+export default {
+  name: "DetailBaseInfo",
+  props: {
+    goodsInfo: {
+      type: Object,
+      default() {
+        return {};
       }
     }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
